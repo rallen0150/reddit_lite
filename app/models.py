c5215574cbf629e226e61aeaaab0dc4a5997e474
rallen0150 <rallen0150@gmail.com>
2016-10-12 18:05:20 -0400

    finally got a day count

---
+++ app/models.py
@@ -0,0 +1,50 @@
+from django.db import models
+from django.contrib.auth.models import User
+import datetime
+from django.utils import timezone
+
+
+# Create your models here.
+class Subreddit(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=255)
+    creation = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+    @property
+    def current_count(self):
+        return Post.objects.filter(subreddit=self).count()
+
+    def today_count(self):
+        x = datetime.datetime.now() - datetime.timedelta(days=1)
+        return Post.objects.filter(subreddit=self).filter(post_created__gte=x).count()
+
+    def daily_avg(self):
+        pass
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    post_description = models.CharField(max_length=255)
+    url_page = models.URLField(null=True, blank=True)
+    post_created = models.DateTimeField(auto_now_add=True)
+    post_edit = models.DateTimeField(auto_now=True)
+    subreddit = models.ForeignKey(Subreddit)
+    user = models.ForeignKey(User)
+
+    def __str__(self):
+        return self.title
+
+    def is_recent(self):
+        pass
+
+    def is_hot(self):
+        pass
+
+class Comment(models.Model):
+    comment = models.CharField(max_length=255)
+    comment_created = models.DateTimeField(auto_now_add=True)
+    comment_edit = models.DateTimeField(auto_now=True)
+    comment_user = models.ForeignKey(User)
+    comment_post = models.ForeignKey(Post)
---
+++ app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from app.models import Subreddit, Post, Comment
+
+# Create your views here.
+def index_view(request):
+    context = {
+        "all_sub": Subreddit.objects.all()
+    }
+    return render(request, "index.html", context)
