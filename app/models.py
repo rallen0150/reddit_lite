598c3a001e405b9bf379eba616b8f18d4d9ae16a
rallen0150 <rallen0150@gmail.com>
2016-10-15 22:18:55 -0400

    going to fix timezone problem

--- app/models.py
+++ app/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from datetime import datetime, timedelta
-
+from django.utils import timezone
 
 # Create your models here.
 class Subreddit(models.Model):
