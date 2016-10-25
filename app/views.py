037b28a022371428722e2404803d1490f000ca61
rallen0150 <rallen0150@gmail.com>
2016-10-25 15:53:44 -0400

    cleaning it up

--- app/views.py
+++ app/views.py
@@ -4,6 +4,7 @@ from app.models import Subreddit, Post, Comment
 from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse, reverse_lazy
 # Create your views here.
 
 def index_view(request):
@@ -12,20 +13,28 @@ def index_view(request):
 class UserCreateView(CreateView):
     model = User
     form_class = UserCreationForm
-    success_url = "/subreddit"
+    success_url = reverse_lazy("index_view")
 
 class SubredditView(ListView):
     model = Subreddit
 
 class SubredditCreateView(CreateView):
     model = Subreddit
-    success_url = "/subreddit"
+    success_url = reverse_lazy('subreddit_view')
     fields = ('name', 'description')
+
+    # def get_success_url(self, **kwargs):
+    #     return reverse_lazy('subreddit_detail_view', args=[int(self.kwargs['pk'])])
+
 
 class SubredditUpdateView(UpdateView):
     model = Subreddit
-    success_url = "/subreddit"
+    # success_url = "/subreddit"
     fields = ('name', 'description')
+
+    def get_success_url(self, **kwargs):
+        return reverse_lazy('subreddit_detail_view', args=[int(self.kwargs['pk'])])
+
 
 class SubredditDetailView(DetailView):
     model = Subreddit
@@ -35,8 +44,12 @@ class PostDetailView(DetailView):
 
 class PostCreateView(CreateView):
     model = Post
-    success_url = '/subreddit'
+    # success_url = '/subreddit'
+    # success_url = reverse_lazy("subreddit_view")
     fields = ['title', 'post_description', 'url_page']
+
+    def get_success_url(self, **kwargs):
+        return reverse_lazy('subreddit_detail_view', args=[int(self.kwargs['pk'])])
 
     def form_valid(self, form):
         instance = form.save(commit=False)
@@ -46,14 +59,20 @@ class PostCreateView(CreateView):
 
 class PostUpdateView(UpdateView):
     model = Post
-    success_url = '/subreddit'
+    # success_url = '/subreddit'
     fields = ('title', 'post_description')
+
+    def get_success_url(self, **kwargs):
+        return reverse_lazy('post_update_view', args=[int(self.kwargs['pk'])])
 
 
 class CommentCreateView(CreateView):
     model = Comment
-    success_url = '/subreddit'
+    # success_url = '/subreddit'
     fields = ('comment', )
+
+    def get_success_url(self, **kwargs):
+        return reverse_lazy('post_detail_view', args=[int(self.kwargs['pk'])])
 
     def form_valid(self, form):
         instance = form.save(commit=False)
@@ -63,5 +82,8 @@ class CommentCreateView(CreateView):
 
 class CommentUpdateView(UpdateView):
     model = Comment
-    success_url = '/subreddit'
+    # success_url = '/subreddit'
     fields = ('comment', )
+
+    def get_success_url(self, **kwargs):
+        return reverse_lazy('post_detail_view', args=[int(self.kwargs['pk'])])
